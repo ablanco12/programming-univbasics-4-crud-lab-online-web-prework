a53716c4c23233f85af8ea4ec92aa44ad3cda169
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-07-23 01:02:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,69 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  number = ["zero","one","two","three"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  number = ["zero", "one", "two", "three"]
+ 
+  number.push("zero")
+ 
+  p number
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  number = ["zero", "one", "two", "three"]
+ 
+  number.unshift("three")
+ 
+  p number
 end
 
 def remove_element_from_end_of_array(array)
+  number = ["zero", "one", "two", "three"]
   
+  pop("three")
+  
+  p number
 end
 
 def remove_element_from_start_of_array(array)
+  number = ["zero", "one", "two", "three"]
   
+  number.shift("zero")
+  
+  p number
 end
 
 def retrieve_element_from_index(array, index_number)
+  number = ["zero", "one", "two", "three"]
   
+  number[0]
+  
+  p number
 end
 
 def retrieve_first_element_from_array(array)
+  number = ["zero", "one", "two", "three"]
   
+  number[0]
 end
 
 def retrieve_last_element_from_array(array)
+  number = ["zero", "one", "two", "three"]
   
+  number[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  number = ["zero", "one", "two", "three"]
+  
+  index_number[0]
+  element()
+  
+  
+  p number
 end
