064687fed78a44ea2ef31cde1687b17174a32391
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-07-23 01:07:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -45,8 +45,6 @@ def retrieve_element_from_index(array, index_number)
   number = ["zero", "one", "two", "three"]
   
   number[0]
-  
-  p number
 end
 
 def retrieve_first_element_from_array(array)
